befc6bbf790488481a70d7cdf3cd30efdc6f2a8d
Gargantua7 <gargantua7@qq.com>
2025-12-02 20:19:39 +0800

    Day.2.GiftShop

---
+++ src/main/kotlin/solution/Day.2.GiftShop.I.kt
@@ -0,0 +1,19 @@
+package solution
+
+import inputs
+
+//https://adventofcode.com/2025/day/2
+fun main() {
+    inputs("Day.2.GiftShop").first().split(",")
+        .map {
+            it.split("-")
+                .map(String::toLong)
+        }.sumOf { (s, e) ->
+            (s..e).sumOf { n ->
+                val s = n.toString()
+                if (s.length % 2 == 0 && s.take(s.length / 2) == s.takeLast(s.length / 2)) {
+                    n
+                } else 0
+            }
+        }.let(::println)
+}
---
+++ src/main/kotlin/solution/Day.2.GiftShop.II.kt
@@ -0,0 +1,27 @@
+package solution
+
+import inputs
+
+//https://adventofcode.com/2025/day/2
+fun main() {
+    inputs("Day.2.GiftShop").first().split(",")
+        .map {
+            it.split("-")
+                .map(String::toLong)
+        }.sumOf { (s, e) ->
+            (s..e).sumOf { n ->
+                val s = n.toString()
+
+                judge@ for (len in 1..s.length / 2) {
+                    if (s.length % len == 0) {
+                        for (i in len..s.lastIndex) {
+                            if (s[i] != s[i - len]) continue@judge
+                        }
+                        return@sumOf n
+                    }
+                }
+
+                0L
+            }
+        }.let(::println)
+}
