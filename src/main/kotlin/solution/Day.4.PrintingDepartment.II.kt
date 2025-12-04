86ca3850f7fb21674ed4d29f679718551d6f99d1
Gargantua7 <gargantua7@qq.com>
2025-12-04 19:39:00 +0800

    Day.4.PrintingDepartment

---
+++ src/main/kotlin/solution/Day.4.PrintingDepartment.II.kt
@@ -0,0 +1,40 @@
+package solution
+
+import inputs
+
+// https://adventofcode.com/2025/day/4
+fun main() {
+    val matrix = inputs("Day.4.PrintingDepartment").map {
+        it.toCharArray()
+    }.toList()
+
+    val direction = arrayOf(
+        -1 to -1, -1 to 0, -1 to 1,
+        0 to -1, 0 to 1,
+        1 to -1, 1 to 0, 1 to 1
+    )
+
+    var ans = 0
+
+    do {
+        var curr = 0
+        for (row in matrix.indices) {
+            for (col in matrix[row].indices) {
+                if (matrix[row][col] == '@') {
+                    val count = direction.count { (dx, dy) ->
+                        matrix.getOrNull(row + dx)
+                            ?.getOrNull(col + dy) == '@'
+                    }
+
+                    if (count < 4) {
+                        matrix[row][col] = '.'
+                        curr++
+                    }
+                }
+            }
+        }
+        ans += curr
+    } while (curr > 0)
+
+    println(ans)
+}
